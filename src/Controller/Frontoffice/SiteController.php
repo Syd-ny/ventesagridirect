659c25c8e2be4d17abecd004f49fc583bd5c7e6b
Syd-ny <sydneymillon@gmail.com>
2023-10-03 18:06:50 +0200

    colonnes catégories et marques sur page d'accueil

--- src/Controller/Frontoffice/SiteController.php
+++ src/Controller/Frontoffice/SiteController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ProductRepository;
+use App\Entity\Categorie;
+use App\Entity\Marque;
 
 class SiteController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil")
      */
-    public function accueil(ProductRepository $productRepository): Response
+    public function accueil(ProductRepository $productRepository, EntityManagerInterface $entityManager): Response
     {
+        $categorie = $entityManager->getRepository(Categorie::class)->findAll();
+
+        $marque = $entityManager->getRepository(Marque::class)->findAll();
+
         // Spécifie l'ordre pour récupérer les produits triés
         $order = 'ASC';
         // Récupére les trois produits avec l'ordre défini depuis la base de données
@@ -22,6 +29,8 @@ class SiteController extends AbstractController
         return $this->render('frontoffice/accueil.html.twig', [
             'controller_name' => 'MainController',
             'products' => $products,
+            'categorie' => $categorie,
+            'marque' => $marque,
         ]);
     }
 }

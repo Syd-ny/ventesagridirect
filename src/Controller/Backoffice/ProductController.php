0fa37f3b2acc77932d033c1c4dcbcf25fd814d94
Syd-ny <sydneymillon@gmail.com>
2023-10-04 17:52:53 +0200

    Affichage des produits pour chaque catégorie avec redirection + mise en page

--- src/Controller/Backoffice/ProductController.php
+++ src/Controller/Backoffice/ProductController.php
@@ -138,4 +138,5 @@ class ProductController extends AbstractController
             self::PRODUCT => $product,
         ]);
     }
+
 }
--- src/Controller/Frontoffice/SiteController.php
+++ src/Controller/Frontoffice/SiteController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Frontoffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ProductRepository;
+use App\Repository\CategorieRepository;
 use App\Entity\Categorie;
 use App\Entity\Marque;
 
@@ -31,6 +32,28 @@ class SiteController extends AbstractController
             'products' => $products,
             'categorie' => $categorie,
             'marque' => $marque,
+        ]);
+    }
+
+    /**
+     * @Route("/categories/{categoryId}/products", name="products_by_category")
+     */
+    public function listProductsByCategories(int $categoryId, CategorieRepository $categorieRepository): Response
+    {
+
+        $category = $categorieRepository->find($categoryId);
+
+
+        if (!$category) {
+            throw $this->createNotFoundException('La catégorie spécifiée n\'existe pas.');
+        }
+
+        // Vous devez également récupérer les produits liés à cette catégorie ici
+        $products = $category->getProducts();
+
+        return $this->render('frontoffice/categorie/list_products_by_categories.html.twig', [
+            'category' => $category,
+            'products' => $products,
         ]);
     }
 }
